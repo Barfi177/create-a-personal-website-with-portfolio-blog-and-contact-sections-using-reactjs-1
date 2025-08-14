9ee76d37c7f277ae0d3f1aba4f1326b68984b6f8
Barfi177 <alabarfi99@gmail.com>
2025-08-14 11:13:18 +0600

    Add src/utils/helper8.js - Generated by Adorable AI

---
+++ src/utils/helper8.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: 'proj-1',
+    title: 'Project One',
+    description: 'A short description of project one built with React.',
+    demo: 'https://example.com',
+    repo: 'https://github.com/you/project-one',
+    image: '/images/project1.png'
+  },
+  {
+    id: 'proj-2',
+    title: 'Project Two',
+    description: 'A short description of project two.',
+    repo: 'https://github.com/you/project-two'
+  }
+];
+
+export default projects;
