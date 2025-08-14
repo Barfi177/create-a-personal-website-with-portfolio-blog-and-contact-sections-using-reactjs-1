37eef344a7336371d84ceff5ff8f855c336e717a
Barfi177 <alabarfi99@gmail.com>
2025-08-14 11:13:24 +0600

    Add src/utils/helper16.js - Generated by Adorable AI

---
+++ src/utils/helper16.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: 'proj-1',
+    title: 'Project One',
+    description: 'Short description of project one.',
+    demo: 'https://example.com',
+    repo: 'https://github.com/you/project-one',
+    image: '/images/project1.png'
+  },
+  {
+    id: 'proj-2',
+    title: 'Project Two',
+    description: 'Short description of project two.',
+    repo: 'https://github.com/you/project-two'
+  }
+];
+
+export default projects;
